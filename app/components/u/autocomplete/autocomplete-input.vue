4b0e713b3231003a9619c8804377aad487a345a4
jamie <hi@jamie.to>
2026-03-19 16:16:09 -0400

    feat: add autocomplete components

---
+++ app/components/u/autocomplete/autocomplete-input.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { AutocompleteInputEmits, AutocompleteInputProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { cn, interactiveStyles, staticStyles } from '#imports'
+import { AutocompleteInput, useForwardPropsEmits } from 'reka-ui'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const props = withDefaults(defineProps<AutocompleteInputProps & {
+  class?: HTMLAttributes['class']
+  showIcon?: boolean
+}>(), {
+  class: '',
+  showIcon: true,
+})
+
+const emits = defineEmits<AutocompleteInputEmits>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
+
+<template>
+  <div
+    data-slot="autocomplete-input-wrapper"
+    :class="cn(staticStyles.base, interactiveStyles.size.default, 'flex items-center gap-2 px-0 w-full', props.class)"
+  >
+    <Icon
+      v-if="props.showIcon"
+      name="mingcute:search-line"
+      class="opacity-50 shrink-0 size-4"
+    />
+    <AutocompleteInput
+      data-slot="autocomplete-input"
+      :class="cn(
+        'flex-1 outline-hidden ',
+        props.class,
+      )"
+      v-bind="{ ...$attrs, ...forwarded }"
+    >
+      <slot />
+    </AutocompleteInput>
+  </div>
+</template>
---
+++ app/components/u/autocomplete/autocomplete-trigger.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { ComboboxTriggerProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { cn, interactiveStyles } from '#imports'
+import { AutocompleteTrigger, useForwardProps } from 'reka-ui'
+
+const props = withDefaults(
+  defineProps<ComboboxTriggerProps & { class?: HTMLAttributes['class'], size?: 'sm' | 'default' }>(),
+  { size: 'default' },
+)
+
+const delegatedProps = reactiveOmit(props, 'class', 'size')
+const forwarded = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <AutocompleteTrigger
+    data-slot="autocomplete-trigger"
+    :data-size="size"
+    v-bind="forwarded"
+    :class="cn(
+      interactiveStyles.base,
+      interactiveStyles.variant.ghost,
+      interactiveStyles.size.icon,
+      'mr-2',
+      props.class,
+
+    )"
+  >
+    <slot>
+      <Icon name="mingcute:down-line" class="text-muted-foreground translate-y-0.5 size-3.5!" />
+    </slot>
+  </AutocompleteTrigger>
+</template>
