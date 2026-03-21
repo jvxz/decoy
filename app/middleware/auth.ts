75d59f1d1135d0221baf938358128007c2e7fe4c
jamie <hi@jamie.to>
2026-03-21 15:21:31 -0400

    feat: add auth middleware

---
+++ app/middleware/auth.ts
@@ -0,0 +1,13 @@
+export default defineNuxtRouteMiddleware(async (to) => {
+  if (import.meta.server)
+    return
+
+  const clientStore = useClientStore()
+  const unauthed = !clientStore.client.getUserId()
+
+  if (unauthed && to.path.includes('/app'))
+    return navigateTo('/login')
+
+  if (!unauthed && to.path.includes('/login'))
+    return navigateTo('/app')
+})
