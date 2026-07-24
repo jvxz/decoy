9e3b94a9a72ab60557d6f619855a4aad08795d6a
Jamie <hi@jamie.to>
2026-07-24 13:12:56 -0400

    feat: edit header title component

--- app/components/layout/app/aside/header-title.vue
+++ app/components/layout/app/aside/header-title.vue
@@ -1,5 +1,13 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+
+const props = defineProps<{ class?: HTMLAttributes['class'] }>()
+
+const delegated = reactiveOmit(props, 'class')
+</script>
+
 <template>
-  <h2 class="text-lg font-medium" v-bind="$attrs">
+  <h2 v-bind="delegated" :class="cn('font-medium p-2.5 px-4 flex size-full items-center', props.class)">
     <slot>
       {{ upperFirst($route.name) }}
     </slot>
