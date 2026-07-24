71835a40a1ba03e331122a72ed0cc14c1c7bf1ca
Jamie <hi@jamie.to>
2026-07-24 14:50:30 -0400

    feat: add `last-active` + `verified` sort options

--- app/components/u/sort-select/root.vue
+++ app/components/u/sort-select/root.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import { createContext } from 'reka-ui'
 
-export const SORT_SELECT_OPTIONS = ['name', 'date-modified', 'date-created'] as const
+export const SORT_SELECT_OPTIONS = ['name', 'date-modified', 'date-created', 'last-active', 'verified'] as const
 export const SORT_SELECT_DIRS = ['asc', 'desc'] as const
 export type SortSelectOption = (typeof SORT_SELECT_OPTIONS)[number]
 export type SortSelectDir = (typeof SORT_SELECT_DIRS)[number]
