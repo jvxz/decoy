98849a795d53bc743166b9b2f59b9713e6600447
Jamie <hi@jamie.to>
2026-08-08 13:36:58 -0400

    feat: add generic error + supported UIA stages constants

--- shared/utils/constants.ts
+++ shared/utils/constants.ts
@@ -1,6 +1,9 @@
-import { EventType } from 'matrix-js-sdk'
+import { AuthType, EventType } from 'matrix-js-sdk'
 
+import type { ErrorShape } from '../../app/utils/error'
 import type { AvatarImageSize } from '../../app/utils/matrix/types'
+
+import { ErrorCode } from '../../app/utils/error'
 
 export const appMeta = {
   description: 'A familiar Matrix client for humans',
@@ -37,6 +40,16 @@ export const ASIDE_DISPLAY_MODES = {
 }
 
 export const MATRIX = {
+  AUTH: {
+    UIA: {
+      SUPPORTED_STAGES: new Set<AuthType | (string & {})>([
+        AuthType.Password,
+        AuthType.Recaptcha,
+        AuthType.Sso,
+        AuthType.Email,
+      ]),
+    },
+  },
   MESSAGING: {
     ALLOWED_ATTRS: [
       'href',
@@ -115,3 +128,11 @@ export const MATRIX = {
     },
   },
 } as const
+
+export const GENERIC_ERROR = {
+  INVALID_HOMESERVER: {
+    code: ErrorCode.InvalidHomeserver,
+    message: 'The provided homeserver is invalid. Please ensure the URL provided is correct with no misspellings.',
+    title: 'Invalid homeserver',
+  },
+} satisfies Record<string, ErrorShape>
