ae4c240fee377e3e39d12fd3e3df5d858e6edec6
Jamie <hi@jamie.to>
2026-08-09 13:53:13 -0400

    feat: enhance `useAuth` + add register mutation

--- app/composables/use-auth.ts
+++ app/composables/use-auth.ts
@@ -1,4 +1,4 @@
-import type { ICreateClientOpts, LoginResponse, LoginRequest as MatrixLoginRequest } from 'matrix-js-sdk'
+import type { ICreateClientOpts, LoginResponse, MatrixClient, LoginRequest as MatrixLoginRequest } from 'matrix-js-sdk'
 import type { MatrixError } from 'matrix-js-sdk'
 
 import { createClient } from 'matrix-js-sdk'
@@ -30,9 +30,12 @@ export type LoginRequest = Prettify<PasswordLoginRequest | TokenLoginRequest>
 
 export function useAuth() {
   const { client } = useMatrixClient()
+  const { attemptAction } = useInteractiveAuth()
 
-  const login = useAsyncState(
-    async (req: LoginRequest) => {
+  const setAuthData = (authPayload: AuthPayload) => idb.setItem<AuthPayload>('auth', authPayload)
+
+  const login = useMutation({
+    mutationFn: async (req: LoginRequest) => {
       const homeserver =
         req.type === 'm.login.password'
           ? await resolveHomeserverBaseUrl(normalizeHomeserverUrl(req.baseUrl))
@@ -61,18 +64,64 @@ export function useAuth() {
         refreshToken: loginRes.refresh_token,
         userId: loginRes.user_id,
       }
-      await idb.setItem<AuthPayload>('auth', authPayload)
+      await setAuthData(authPayload)
 
       return authPayload
     },
-    undefined,
-    { immediate: false, throwError: true },
-  )
+    mutationKey: $mk.login(),
+  })
+
+  const register = useMutation<
+    AuthPayload,
+    EmailInUseError | EmailRateLimitedError,
+    {
+      manualClient?: MatrixClient
+      email: string
+      password: string
+      username: string
+    }
+  >({
+    mutationFn: async ({ email, password, username, manualClient }) => {
+      const matrixClient = manualClient ?? client.value
+
+      const { inUse, sid } = await isEmailInUse(matrixClient, email, crypto.randomUUID())
+      if (inUse) {
+        throw new EmailInUseError()
+      }
+
+      const res = await attemptAction(
+        auth => matrixClient.registerRequest({ auth: auth ?? undefined, password, username }),
+        {
+          emailSid: sid,
+          inputs: { emailAddress: email },
+          matrixClient,
+          requestEmailToken: (addr, secret, attempt) => matrixClient.requestRegisterEmailToken(addr, secret, attempt),
+        },
+      )
+
+      assert(res.access_token, 'Expected access token')
+
+      const payload: AuthPayload = {
+        accessToken: res.access_token,
+        baseUrl: matrixClient.getHomeserverUrl(),
+        deviceId: res.device_id,
+        refreshToken: res.refresh_token,
+        userId: res.user_id,
+      }
+
+      await setAuthData(payload)
+
+      return payload
+    },
+    mutationKey: $mk.register(),
+  })
 
   const logout = useAsyncState(async () => logoutClient(client.value), undefined, { immediate: false })
 
   return {
     login,
     logout,
+    register,
+    setAuthData,
   }
 }
