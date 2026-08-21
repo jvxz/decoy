482de2f72c2bb2eae27b06405345f6b80a40cac4
Jamie <hi@jamie.to>
2026-08-21 14:25:33 -0400

    feat: add p tag regex + move emoji regex to shared file

--- shared/utils/regex.ts
+++ shared/utils/regex.ts
@@ -2,3 +2,7 @@ export const A_TO_Z_RE = /[a-z]/i
 export const ALPHANUMERIC_RE = /^[a-z0-9]+$/
 export const WHITESPACE_RE = /\s+/
 export const TRAILING_NEWLINE_RE = /\n$/
+export const P_TAG_RE = /^<p>(.*)<\/p>$/s
+
+export const EMOJI_RE = /\p{RGI_Emoji}/gv
+export const EMOJI_VARIATION_RE = /[\uFE00-\uFE0F]/gu
