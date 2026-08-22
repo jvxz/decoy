5f7781e0b495e73f0c8fa09a563630a46772f62f
Jamie <hi@jamie.to>
2026-08-22 16:59:55 -0400

    feat: enhance `useRoomMemberPowerLevel` composable

--- app/composables/use-room-member-power-level.ts
+++ app/composables/use-room-member-power-level.ts
@@ -1,8 +1,24 @@
+import { EventType } from 'matrix-js-sdk'
+
 export function useRoomMemberPowerLevel(
   roomId: MaybeRefOrGetter<MaybeRoomOrId | undefined>,
   maybeUserOrId: MaybeRefOrGetter<MaybeUserOrId | undefined>,
 ) {
   const roomMember = useRoomMember(roomId, maybeUserOrId)
+  const roomPowerLevels = useRoomPowerLevels(roomId)
 
-  return computed(() => roomMember.value?.powerLevel)
+  const powerLevel = computed(() => roomMember.value?.powerLevel ?? 0)
+  const powerLevelName = computed(() => getPowerLevelName(powerLevel.value))
+
+  const allowed = (value: number | undefined) => (value !== undefined ? value <= powerLevel.value : false)
+
+  const canRedact = computed(() => allowed(roomPowerLevels.value?.redact))
+  const canRedactSelf = computed(() => allowed(roomPowerLevels.value?.events?.[EventType.RoomRedaction]))
+
+  return {
+    canRedact,
+    canRedactSelf,
+    powerLevel,
+    powerLevelName,
+  }
 }
