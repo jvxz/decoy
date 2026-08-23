5474530ca5460cee29dbc7880ba2d69219fe2ff8
Jamie <hi@jamie.to>
2026-08-23 13:54:01 -0400

    feat: add mention + blockquote components

---
+++ app/components/u/blockquote.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface UBlockquoteProps extends PrimitiveProps {
+  class?: HTMLAttributes['class']
+}
+
+const props = withDefaults(defineProps<UBlockquoteProps>(), {
+  as: 'blockquote',
+})
+
+const delegated = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <Primitive
+    v-bind="delegated"
+    :class="cn('text-muted-foreground text-sm flex items-center gap-2 h-2em', props.class)"
+    data-slot="blockquote"
+  >
+    <span aria-hidden="true" class="w-px h-full bg-border" />
+
+    <span>
+      <slot />
+    </span>
+  </Primitive>
+</template>
---
+++ app/components/u/mention.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import type { PrimitiveProps } from 'reka-ui'
+import type { ButtonHTMLAttributes, HTMLAttributes } from 'vue'
+
+import type { ButtonProps } from './button.vue'
+
+export interface UMentionProps extends PrimitiveProps {
+  type: MentionType
+  class?: HTMLAttributes['class']
+  size?: ButtonProps['size']
+  onClick?: ButtonHTMLAttributes['onClick']
+}
+
+const props = defineProps<UMentionProps>()
+
+const delegated = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <UButton
+    v-bind="delegated"
+    :class="
+      cn(
+        'px-1 py-0 border-none bg-primary/75 opacity-80 px-1 -m-0.25 mx-0.75 text-base h-fit rounded text-foreground',
+        'active:bg-primary',
+        'hover:(bg-primary opacity-100)',
+        'data-[popover-open]:(bg-primary opacity-100)',
+        props.class,
+      )
+    "
+    data-slot="mention"
+  >
+    <slot />
+  </UButton>
+</template>
