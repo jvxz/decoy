a52191b32b1b0f576446d6fd2e69b0058e67f574
Jamie <hi@jamie.to>
2026-08-24 14:03:09 -0400

    feat: enhance name resolution in direct room page

--- app/pages/app/me/[directRoomId].vue
+++ app/pages/app/me/[directRoomId].vue
@@ -12,15 +12,22 @@ definePageMeta({
 })
 
 const route = useRoute()
-const room = useRoom(() => route.params.directRoomId)
+const roomId = computed(() => route.params.directRoomId)
 
-defineAppLabel({ label: () => (room.value ? resolveRoomName(room.value) : route.params.directRoomId) })
+const room = useRoom(roomId)
+const { data: summary } = useRoomSummary(roomId)
+
+const resolvedName = computed(() =>
+  room.value ? resolveRoomName(room.value) : summary.value ? summary.value.name : roomId.value,
+)
+
+defineAppLabel({ label: resolvedName })
 </script>
 
 <template>
   <LayoutAppSlot name="page-header">
     <LayoutAppPageHeader>
-      <span> {{ room ? resolveRoomName(room) : 'Unknown room' }}</span>
+      <span> {{ resolvedName }}</span>
     </LayoutAppPageHeader>
   </LayoutAppSlot>
 
