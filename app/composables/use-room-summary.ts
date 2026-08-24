cef87b24da0be238b8bdaa3a203b267f196551d0
Jamie <hi@jamie.to>
2026-08-24 14:02:24 -0400

    feat: enhance `useRoomSummary`

--- app/composables/use-room-summary.ts
+++ app/composables/use-room-summary.ts
@@ -1,17 +1,19 @@
-export const useRoomSummary = createProvidableComposable(
-  'useRoomSummary',
-  (roomOrId: MaybeRefOrGetter<MaybeRoomOrId | undefined>) => {
-    const roomId = useResolveRoomId(roomOrId)
-    const { client } = useMatrixClient()
+export function useRoomSummary(roomOrId: MaybeRefOrGetter<MaybeRoomOrId | undefined>) {
+  const roomId = useResolveRoomId(roomOrId)
+  const { client } = useMatrixClient()
 
-    return useQuery({
-      queryFn: () => {
-        if (!roomId.value) return null
-        const { serverName } = parseRoomId(roomId.value) ?? {}
-        return client.value.getRoomSummary(roomId.value, serverName ? [serverName] : [])
-      },
-      queryKey: $qk.roomSummary(roomId),
-      refetchOnWindowFocus: true,
-    })
-  },
-)
+  return useQuery({
+    queryFn: () => {
+      if (!roomId.value) return null
+      const { serverName } = parseRoomId(roomId.value) ?? {}
+      return client.value.getRoomSummary(roomId.value, serverName ? [serverName] : [])
+    },
+    queryKey: $qk.roomSummary(roomId),
+    refetchOnWindowFocus: true,
+    retry: (count, err) => {
+      if (isMatrixError(err)) return err.errcode !== MatrixErrorCode.M_NOT_FOUND
+
+      return count <= 4
+    },
+  })
+}
