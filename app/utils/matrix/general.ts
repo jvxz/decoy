97c9fabdd7c4ff3f1fab9bff3ad04d5c0c0fc49e
Jamie <hi@jamie.to>
2026-08-25 13:48:56 -0400

    feat: add `parseMatrixToUrl` util

--- app/utils/matrix/general.ts
+++ app/utils/matrix/general.ts
@@ -5,3 +5,31 @@ export function getMatrixIdType(id: string | undefined): 'user' | 'room' | 'unkn
   if (isRoomId(id)) return 'room'
   return 'unknown'
 }
+
+export interface MatrixToUrl {
+  type: 'userId' | 'roomId' | 'roomAlias' | 'event' | 'unknown'
+  action?: 'join' | 'chat'
+  via?: string[]
+}
+export function parseMatrixToUrl(url: string): MatrixToUrl {
+  const { pathname } = parseURL(url)
+  const type = pathname.includes('/!')
+    ? 'roomId'
+    : pathname.includes('/@')
+      ? 'userId'
+      : pathname.includes('/#')
+        ? 'roomAlias'
+        : pathname.includes('/$')
+          ? 'event'
+          : 'unknown'
+
+  const query = parseQuery(url)
+  const via = Array.isArray(query.via) ? query.via : [query.via ?? '']
+  const action = (Array.isArray(query.action) ? query.action[0] : query.action) as 'join' | 'chat' | undefined
+
+  return {
+    action,
+    type,
+    via,
+  }
+}
