b2c9c791a7a1c4929fdfbe48c10f506667f983d1
Jamie <hi@jamie.to>
2026-09-02 15:09:40 -0400

    feat: enhance `useSelf` + `useUser`

--- app/composables/use-self.ts
+++ app/composables/use-self.ts
@@ -1,13 +1,8 @@
 export const useSelf = createGlobalState(() => {
   const { client } = useMatrixClient()
-  const { onSync, onUserProfile } = useMatrixHooks()
 
   const userId = computed(() => client.value.getUserId() ?? undefined)
   const self = useUser(userId)
-  const profile = useUserProfile(userId)
 
-  onUserProfile(self.trigger)
-  onSync(self.trigger)
-
-  return { profile, self }
+  return { self }
 })
